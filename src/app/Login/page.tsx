c0aeb12f90f184314d8e05c271d8f6c516510677
glayberson <glaybersonsilva@outlook.com>
2025-05-18 09:40:00 -0300

    Prototipo login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -8,16 +8,14 @@ function page() {
         <Navbar/>
         <div className='flex flex-1 flex-col items-center justify-center center w-full'>
             <form  className='bg-[#e9e4e9] text-[#000000] flex flex-col rounded-xl p-5 gap-5 border-2 text-center border-black'>
-                <h2 className=''>Cadastro</h2>
-                <input type='text' placeholder='Seu Nome' required className='border border-black rounded-xl p-2'/>
-                <input type='date' placeholder='Data de Nascimento'required className='border border-black rounded-xl p-2'/>
-                <input type='text' placeholder='Telefone' required className='border border-black rounded-xl p-2'/>
+                <h2 className=''>Login</h2>
+          
                 <input type="email" placeholder='Email' required className='border border-black rounded-xl p-2'/>
                 <input type="password" placeholder='Senha'required className='border border-black rounded-xl p-2'/>
-                <button className='rounded-xl border border-black p-2 bg-[#fa1e0b] text-black hover:bg-[#e9e4e9]' type='submit'>Criar sua Conta!!</button>
+                <button className='rounded-xl border border-black p-2 bg-[#fa1e0b] text-black hover:bg-[#e9e4e9]' type='submit'>Entrar</button>
             </form>
             <div className='text-black p-5'>
-                <h1>Ja possui uma conta ?? <Link href="/Login">Entre agora</Link> </h1>
+                <h1>Ainda não tem uma conta ?? <Link href="/Cadastro">Faça agora</Link> </h1>
             </div>
         </div>
     </div>
